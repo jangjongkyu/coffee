0d1ffdb540119800ef56f79e6b6d38cf2fef4b61
ppark0723 <ppark0723@naver.com>
2017-11-15 14:30:18 +0900

    Update coffee.java

--- coffee/src/coffee/coffee.java
+++ coffee/src/coffee/coffee.java
@@ -3,5 +3,7 @@ package coffee;
 public class coffee {
 	public static void main(String[] args) {
 		System.out.println("coffee");
+		System.out.println("jal ge nae sim ni kka?");
+		System.out.println("kkkkkkkkkk");
 	}
 }
